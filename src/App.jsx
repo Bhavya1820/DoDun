f21026708ee04e56a349a0f7f3182fb10d4ee45e
Bhavya1820 <bhavyasoni1820@gmail.com>
2025-02-01 00:08:37 +0530

    about, contact, home page complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from "react";
 import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./components/Home/home";
+import Home from "./components/pages/home";
+import AboutUs from "./components/pages/aboutUs";
+import Blog from "./components/pages/blog";
+import ContactUs from "./components/pages/contactUs";
+import Services from "./components/pages/services";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
+     <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
+    </Router>
     </>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,35 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect  } from "react";
 import { Menu, X } from "lucide-react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+        AOS.init({
+          duration: 1000, // Animation duration in milliseconds
+          once: true, // Whether animation should happen only once
+        });
+      }, []);
 
   return (
-    <nav className="bg-white shadow-md fixed w-full z-50">
+    <nav data-aos="fade-down" className="bg-white shadow-md fixed w-full z-50 relative">
       <div className="container mx-auto flex items-center justify-between p-4">
+        <a href="/">
         <img
           src="https://ik.imagekit.io/jncw2kb8u/LOGO.png?updatedAt=1738224610877"
           alt="Logo"
-          className="h-10"
+          className="h-10 w-30"
+          
         />
+        </a>
         <div className="hidden md:flex space-x-6">
-          <a href="#home" className="text-gray-800 hover:text-blue-500">
+          <a href="/" className="text-gray-800 hover:text-blue-500">
             Home
           </a>
-          <a href="#about" className="text-gray-800 hover:text-blue-500">
+          <a href="/about" className="text-gray-800 hover:text-blue-500">
             About Us
           </a>
-          <a href="#services" className="text-gray-800 hover:text-blue-500">
+          <a href="/services" className="text-gray-800 hover:text-blue-500">
             Services
           </a>
-          <a href="#blog" className="text-gray-800 hover:text-blue-500">
+          <a href="/blog" className="text-gray-800 hover:text-blue-500">
             Blog
           </a>
-          <a href="#contact" className="text-gray-800 hover:text-blue-500">
+          <a href="/contact" className="text-gray-800 hover:text-blue-500">
             Contact Us
           </a>
         </div>
-        <button className="hidden md:block bg-blue-500 text-white px-4 py-2 rounded-lg">
+        <button className="hidden md:block bg-sky-700 text-white px-4 py-2 rounded-lg hover:scale-115 transition duration-300">
           +91-8968881110
         </button>
         <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
@@ -38,23 +50,23 @@ function Navbar() {
       </div>
       {isOpen && (
         <div className="md:hidden bg-white p-4 space-y-2 shadow-lg">
-          <a href="#home" className="block text-gray-800 hover:text-blue-500">
+          <a href="/" className="block text-gray-800 hover:text-blue-500">
             Home
           </a>
-          <a href="#about" className="block text-gray-800 hover:text-blue-500">
+          <a href="/about" className="block text-gray-800 hover:text-blue-500">
             About Us
           </a>
           <a
-            href="#services"
+            href="/services"
             className="block text-gray-800 hover:text-blue-500"
           >
             Services
           </a>
-          <a href="#blog" className="block text-gray-800 hover:text-blue-500">
+          <a href="/blog" className="block text-gray-800 hover:text-blue-500">
             Blog
           </a>
           <a
-            href="#contact"
+            href="/contact"
             className="block text-gray-800 hover:text-blue-500"
           >
             Contact Us
